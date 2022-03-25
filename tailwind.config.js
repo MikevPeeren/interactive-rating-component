e3e523b2aec1c4b1490f145fe675152cc572e397
MikevPeeren <mikevpeeren@hotmail.com>
2022-03-25 21:22:17 +0100

    :recycle: cleaning up to start

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        white: "hsl(0, 0%, 100%)",
+        lightGrey: "hsl(217, 12%, 63%)",
+        mediunGrey: "hsl(216, 12%, 54%)",
+        darkBlue: "hsl(213, 19%, 18%)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+    },
   },
   variants: {
     extend: {},
